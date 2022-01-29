b8a5386213abba793a7914c02affc55d5e53c21b
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2022-01-29 23:00:59 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,25 +1,12 @@
--- SCREEN POSITION PARAMETERS
-local screenPosX = 0.165                    -- X coordinate (top left corner of HUD)
-local screenPosY = 0.882                    -- Y coordinate (top left corner of HUD)
-
-
-Citizen.CreateThread(function()
-	local id = GetPlayerServerId(PlayerId())
-    Wait(50)
+Citizen.CreateThread(function ()
     while true do
-        drawTxt("~w~ID:~b~  ".. id .. '', 4, 0.5, screenPosX, screenPosY + 0.075)
-        Citizen.Wait(1)
+        Citizen.Wait(100)
+        local ped = PlayerPedId()
+        SendNUIMessage({
+            id = GetPlayerServerId(PlayerId()) or 0
+        })
     end
 end)
 
 
-function drawTxt(text, font, scale, x, y)
-    SetTextFont(4)
-    SetTextScale(scale, scale)
-    SetTextDropShadow(0, 0, 0, 0,255)
-    SetTextEdge(1, 0, 0, 0, 255)
-    SetTextOutline()
-    SetTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawText(x, y)
-end
+
